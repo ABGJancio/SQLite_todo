6327f289470a3061683be2ccbbd5fef3b05db1bd
Jan Kondratiuk <abg.projekt@wp.pl>
2021-04-27 21:40:17 +0200

    Todos.html not running

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from forms import TodoForm
 from models import todos
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "nininini"
 
-@app.route("/api/v1/todos/", methods=["GET"])
-def todos_list_api_v1():
-    return todos.select_all()
+
+@app.route("/todos/", methods=["GET", "POST"])
+def todos_list():
+    form = TodoForm()
+    error = ""
+    todos=todos.select_all()
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            todos.add_todo(form.data)
+        return redirect(url_for("todos_list"))
+
+    return render_template("todos.html", form=form, todos=todos, error=error)
+
+
+@app.route("/todos/<int:todo_id>/", methods=["GET", "POST"])
+def todo_details(todo_id):
+    todo = todos.select_id(todo_id - 1)
+    form = TodoForm(data=todo)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            todos.update(todos, todo_id - 1, form.data)
+        return redirect(url_for("todos_list"))
+
+    return render_template("todo.html", form=form, todo_id=todo_id)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
--- models.py
+++ models.py
@@ -9,38 +9,48 @@ class TodosSQLite:
         self.db_file = db_file
 
     def __enter__(self):
-        try:
-            self.conn = sqlite3.connect(db_file)
-        except Error:
-            self.conn = None
-        self.cur = self.conn.cursor()
-        return self.cur
+        # try:
+            self.conn = sqlite3.connect(self.db_file)
+            self.cur = self.conn.cursor()
+            return self.cur
+        # except Error:
+        #     self.conn = None
 
-    def __exit__(self, exc_class, exc, traceback):
+    def __exit__(self, type, value, traceback):
         self.conn.commit()
         self.conn.close()
 
     def create_table(self):
-        with todos as cur:
+        with self as cur:
             cur.execute(
                 '''CREATE TABLE IF NOT EXISTS todos (id integer PRIMARY KEY, title varchar(50), description text, done boolean)''')
 
     def add_todo(self, todo):
-        with todos as cur:
+        with self as cur:
             sql = '''INSERT INTO todos (title, description, done) VALUES(?,?,?)'''
             cur.execute(sql, todo)
         return cur.lastrowid
 
-    def select_all(self, table):
+    def select_all(self):
         try:
-            with todos as cur:
-                sql = f'SELECT * FROM {table}'
+            with self as cur:
+                sql = f'SELECT * FROM todos'
                 cur.execute(sql)
                 rows = cur.fetchall()
                 return rows
         except sqlite3.OperationalError as e:
             print(e)    
         
+    def select_id(self, id):
+        try:
+            with self as cur:
+                sql = f'SELECT * FROM todos WHERE id = {id}'
+                cur.execute(sql)
+                row = cur.fetchone()
+                return row
+        except sqlite3.OperationalError as e:
+            print(e)    
+
 
     def update(self, table, id, **kwargs):
         parameters = [f"{k} = ?" for k in kwargs]
@@ -49,12 +59,12 @@ class TodosSQLite:
         values += (id, )
         sql = f'UPDATE {table} SET {parameters} WHERE id = ?'
         try:
-            with todos as cur:
+            with self as cur:
                 cur.execute(sql, values)
         except sqlite3.OperationalError as e:
             print(e)
 
-    def delete_where(conn, table, **kwargs):
+    def delete_where(self, table, **kwargs):
         qs = []
         values = tuple()
         for k, v in kwargs.items():
@@ -63,22 +73,33 @@ class TodosSQLite:
         q = " AND ".join(qs)
         sql = f'DELETE FROM {table} WHERE {q}'
         try:
-            with todos as cur:
+            with self as cur:
                 cur.execute(sql, values)
         except sqlite3.OperationalError as e:
             print(e)
 
+    def table_col(self):
+        sql = 'PRAGMA table_info(todos)'
+        try:
+            with self as cur:
+                cur.execute(sql)
+                columns = [c[1] for c in cur.fetchall()]
+                return columns
+        except sqlite3.OperationalError as e:
+            print(e)
 
-db_file = "./tasks_todo/todos.db"
-todos = TodosSQLite(db_file)
+# db_file = "./tasks_todo/todos.db"
+# todos = TodosSQLite(db_file)
 
 if __name__ == '__main__':
 
+    db_file = "./tasks_todo/todos.db"
+    todos = TodosSQLite(db_file)
     # todos.create_table()
 
     # todo = ('Szczepienie', 'Zadzwonić do punktu szczepień i zarejestrować się', False)
     # todos.add_todo(todo)
 
-    # todos.update(todos, 3, done=True)
+    # todos.update("todos", 3, done=True)
 
-    print(todos.select_all(todos))
+    # print(todos.table_col())
